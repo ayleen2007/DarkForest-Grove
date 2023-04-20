cd5bf28d8c045f4a3bcf1681831301c66b77a7fd
ayleen2007 <725010@student.dusd.net>
2023-04-20 14:14:15 -0700

    upload

--- DarkForest Grove/Assets/ayleen/playermovement.cs
+++ DarkForest Grove/Assets/ayleen/playermovement.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class playermovement : MonoBehaviour
 {
+    public float moveSpeed;
+    private Rigidbody2D rb;
+    private bool facingRight = true;
     // Start is called before the first frame update
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     void Start()
     {
         
