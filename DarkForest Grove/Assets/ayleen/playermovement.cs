84cef8e9f2228435908ff45436055573e2dcc128
ayleen2007 <725010@student.dusd.net>
2023-04-20 15:22:53 -0700

    upload

--- DarkForest Grove/Assets/ayleen/playermovement.cs
+++ DarkForest Grove/Assets/ayleen/playermovement.cs
@@ -7,19 +7,69 @@ public class playermovement : MonoBehaviour
     public float moveSpeed;
     private Rigidbody2D rb;
     private bool facingRight = true;
+    private float moveDirection;
+    public float jumpForce;
+
+    private bool isJumping = false;
     // Start is called before the first frame update
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
-    void Start()
-    {
-        
-    }
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        //get inputs 
+        ProcessInputs();
+        // animate
+        Animate();
+
+    }
+     private void FixedUpdate()
+    {
+        //move
+        Move();
+    }
+
+    private void Animate()
+    {
+        if (moveDirection > 0 && !facingRight)
+        {
+            FlipCharacter();
+        }
+        else if (moveDirection < 0 && facingRight)
+        {
+            FlipCharacter();
+        }
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+
+        if (isJumping)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+        }
+        isJumping = false;
+    }
+
+    private void ProcessInputs()
+    {
+        moveDirection = Input.GetAxis("Horizontal"); //scale of -1 > 1
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+        }
+    }
+
+    private void FlipCharacter()
+    {
+        facingRight = !facingRight;
+
+        transform.Rotate(0f, 180f, 0f);
     }
 }
