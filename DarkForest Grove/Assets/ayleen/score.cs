e62d05b5b3490a53b9456d793e889ebc4bac23e1
ayleen2007 <725010@student.dusd.net>
2023-04-25 14:24:10 -0700

    upload

---
+++ DarkForest Grove/Assets/ayleen/score.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour
+{
+    public Text MyscoreText;
+    private int ScoreNum;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreNum = 0;
+        MyscoreText.text = "Score : " + ScoreNum;
+    }
+
+    private void OnTriggerEnter2D(Collider2D Coin)
+    {
+        if(Coin.tag == "Mycoin")
+        {
+            ScoreNum += 1;
+            Destroy(Coin.gameObject);
+            MyscoreText.text = "Score" + ScoreNum;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
