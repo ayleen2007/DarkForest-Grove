522987c095bf3dc1bbb7fe7a3325717d15245398
jaredeva <728705@student.dusd.net>
2023-05-08 14:22:43 -0700

    Upload

--- DarkForest Grove/Assets/jared/Enemy.cs
+++ DarkForest Grove/Assets/jared/Enemy.cs
@@ -5,11 +5,42 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public static event Action<Enemy> OnEnemyKilled;
     [SerializeField] float health, maxHealth = 3f;
+
+    [SerializeField] float moveSpeed = 5f;
+    Rigidbody2D rb;
+    Transform target;
+    Vector2 moveDirection;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     private void Start()
     {
         health = maxHealth;
+        target = GameObject.Find("player").transform;
+    }
+
+    private void Update()
+    {
+        if(target)
+        {
+            Vector3 direction = (target.position - transform.position).normalized;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            rb.rotation = angle;
+            moveDirection = direction;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(target)
+        {
+            rb.velocity = new Vector2(moveDirection.x, moveDirection.y) * moveSpeed;
+        }
     }
 
     public void TakeDamage(float damageAmount)
