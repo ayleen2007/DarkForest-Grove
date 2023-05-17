fe82b9c41402daa801574d5986f5ee45aa140406
jaredeva <728705@student.dusd.net>
2023-05-17 14:57:56 -0700

    Upload

--- DarkForest Grove/Assets/jared/Enemy.cs
+++ DarkForest Grove/Assets/jared/Enemy.cs
@@ -21,23 +21,21 @@ public class Enemy : MonoBehaviour
     private void Start()
     {
         health = maxHealth;
-        target = GameObject.Find("player").transform;
+        target = GameObject.Find("Player").transform;
     }
 
     private void Update()
     {
-        if(target)
+        if (target)
         {
             Vector3 direction = (target.position - transform.position).normalized;
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            rb.rotation = angle;
             moveDirection = direction;
         }
     }
 
     private void FixedUpdate()
     {
-        if(target)
+        if (target)
         {
             rb.velocity = new Vector2(moveDirection.x, moveDirection.y) * moveSpeed;
         }
